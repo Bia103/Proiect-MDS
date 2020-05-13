e426fed4f4e0511f5c775c1e86e8e9b9c862797b
Furculesteanu Bianca <48888793+Bia103@users.noreply.github.com>
2020-05-13 20:35:08 +0300

    Update index.php

--- Music Hall of Fame/index.php
+++ Music Hall of Fame/index.php
@@ -44,65 +44,161 @@ if (isset($_POST['uname']) and isset($_POST['pass']) and !empty($_POST['uname'])
 
 
 
-<!-- HTML ZONE -->
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
-
 <head>
-	<title>Login</title>
-	<style type="text/css">
-		#login{
-			height: auto;
-			width: 310px;
-			margin: auto;
-			top: 0;
-			right: 0;
-			bottom: 0;
-			left: 0;
-			margin: auto;
-			font-family: Helvetica;
-			text-align: center;
-		}
-		
-		INPUT {
-			height: 40px;
-			width: 300px;
-			margin-top: 5px;
-			border-radius: 5px;
-			border: 1px solid skyblue;
-		}
-
-		INPUT:HOVER {
-			border: 1px solid blue;
-		}
-
-		.button {
-			background-color: white;
-			width: 150px;
-		}
-		A {
-			text-decoration: none;
-			color: Black;
-		}
-	</style>
+<title>Home</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="Css/index.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+<style>
+input{
+	border-style: dotted;
+	border-radius: 25px;
+	left:100px;
+	opacity: 0.5;
+	width: 500px;
+}
+input .button{
+	width:20px;
+}
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 
+body, html {
+  height: 100%;
+  line-height: 1.8;
+}
+
+
+.bgimg-1 {
+  background-position: center;
+  background-size: cover;
+  background-image: url("/Images/grey-guitar.jpg");
+  min-height: 100%;
+}
+
+.fereastra-bar .fereastra-button {
+  padding: 16px;
+}
+.or{
+}
+.fereastra-display-left{
+	 animation: fadein 2s;
+}
+@keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
+#container{
+	display:none;
+}
+.fereastra-button{
+	border-radius: 25px;
+}
+input:hover {
+  opacity:1;
+}
+#start{
+	    text-shadow: 0 0 10px #93DEFF, 0 0 20px #93DEFF, 0 0 30px #93DEFF, 0 0 40px #93DEFF, 0 0 50px #93DEFF, 0 0 60px #93DEFF, 0 0 70px #93DEFF;
+		text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+}
+</style>
 <body>
-	<div id="container">
+
+<!-- Navbar (sit on top) -->
+
+
+<!-- Sidebar on small screens when clicking the menu icon -->
+
+
+<!-- Header with full-height image -->
+<header class="bgimg-1 fereastra-display-container fereastra-grayscale-min" id="home">
+  <div class="fereastra-display-left fereastra-text-white" style="padding:48px">
+    <span class="fereastra-jumbo fereastra-hide-small" id = "start">Start your journey in the music's world</span><br>
+    
+    <span class="fereastra-large">Log in or make an account</span>
+	<p><a href="#about" class="fereastra-button fereastra-white fereastra-padding-large fereastra-large fereastra-margin-top fereastra-opacity fereastra-hover-opacity-off" onclick="myFunction()" id = "Learn">Learn more and start today</a></p>
+    <p><class="fereastra-button fereastra-white fereastra-padding-large fereastra-large fereastra-margin-top fereastra-opacity fereastra-hover-opacity-off">  
 	<div id="login">
-		<h2>Login</h2>
-		<form action="index.php" method="POST"> 
+		<div id = "container">
+			<h2>Login</h2>
+				<form action="index.php" method="POST"> 
 		<!-- POST: Sends the form-data as an HTTP post transaction. Trimite către index.php -->
-			<input type="text" name="uname" placeholder="Username"><br>
-			<input type="password" name="pass" placeholder="Password"><br><br>
-			<a href="register.php">Register</a><br><br>
-			<input type="submit" class="button" value="Login">
+				<input type="text" name="uname" placeholder="Username"><br>
+				<input type="password" name="pass" placeholder="Password"><br><br>
+				
+				<input type="submit" class="button" value="Login">
+				<h2 class = "or">OR</h2>
+				<a href="register.php">Register</a><br><br>
+		</div>
 		</form>
-	</div>
+	</div></p>
+  </div> 
+  <div class="fereastra-display-bottomleft fereastra-text-grey fereastra-large" style="padding:24px 48px">
+    <i class="fa fa-facebook-official fereastra-hover-opacity"></i>
+    <i class="fa fa-instagram fereastra-hover-opacity"></i>
+    <i class="fa fa-snapchat fereastra-hover-opacity"></i>
+    <i class="fa fa-pinterest-p fereastra-hover-opacity"></i>
+    <i class="fa fa-twitter fereastra-hover-opacity"></i>
+    <i class="fa fa-linkedin fereastra-hover-opacity"></i>
+  </div>
+
+</header>
+
+<!-- About Section -->
+
+
+
+<!-- Contact Section -->
+
+
+<!-- Footer -->
+<footer class="fereastra-black fereastra-padding-64">
+  <a href="#home" class="fereastra-button fereastra-light-grey"><i class="fa fa-arrow-up fereastra-margin-right"></i>To the top</a>
+  <div class="fereastra-xlarge fereastra-section">
+    <i class="fa fa-facebook-official fereastra-hover-opacity"></i>
+    <i class="fa fa-instagram fereastra-hover-opacity"></i>
+    <i class="fa fa-snapchat fereastra-hover-opacity"></i>
+    <i class="fa fa-pinterest-p fereastra-hover-opacity"></i>
+    <i class="fa fa-twitter fereastra-hover-opacity"></i>
+    <i class="fa fa-linkedin fereastra-hover-opacity"></i>
+  </div>
+  
+</footer>
+ 
+<script>
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function fereastra_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function fereastra_close() {
+    mySidebar.style.display = "none";
+}
+function myFunction() {
+  document.getElementById("Learn").style.display = "none";
+  document.getElementById("container").style.display = "block";
+}
+</script>
 
 </body>
-
-<footer>
-</footer>
-
 </html>
