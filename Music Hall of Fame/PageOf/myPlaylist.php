79308e04dee311eedf93a0e61a927b83e7ce8263
Oana-Ivan <49486605+Oana-Ivan@users.noreply.github.com>
2020-05-22 19:23:32 +0300

    Update myPlaylist.php

--- Music Hall of Fame/PageOf/myPlaylist.php
+++ Music Hall of Fame/PageOf/myPlaylist.php
@@ -17,116 +17,56 @@ $result = mysqli_query($link, $sql) or die('Playlist Query failed: ' . mysql_err
 <!-- HTML ZONE -->
 <!DOCTYPE html>
 <html>
-
-<style>
-	form {
-  /* Just to center the form on the page */
-  margin: 0 auto;
-  width: 800px;
-  /* To see the outline of the form */
-  padding: 1em;
-  border: 1px solid #CCC;
-  border-radius: 1em;
-}
-
-form div + div {
-  margin-top: 1em;
-}
-
-label {
-  /* To make sure that all labels have the same size and are properly aligned */
-  display: inline-block;
-  width: 110px;
-  text-align: right;
-}
-
-input, textarea {
-  /* To make sure that all text fields have the same font settings
-     By default, textareas have a monospace font */
-  font: 1em sans-serif;
-
-  /* To give the same size to all text fields */
-  width: 200px;
-  box-sizing: border-box;
-
-  /* To harmonize the look & feel of text field border */
-  border: 1px solid #999;
-}
-
-input:focus, textarea:focus {
-  /* To give a little highlight on active elements */
-  border-color: #000;
-}
-
-textarea {
-  /* To properly align multiline text fields with their labels */
-  vertical-align: top;
-
-  /* To give enough room to type some text */
-  height: 5em;
-}
-
-.button {
-  /* To position the buttons to the same position of the text fields */
-  padding-left: 90px; /* same size as the label elements */
-}
-
-button {
-  /* This extra margin represent roughly the same space as the space
-     between the labels and their text fields */
-  margin-left: .5em;
-}
-	
-</style>
-
-
+<head>
+    <link rel="stylesheet" type="text/css" href="../Css/myPlaylist.css">
+</head>
 
 <body>
-
-<h2>Playlists:</h2>
-<form action="show_playlist.php" method = "POST">
-
-	<table>
-		<tr>
-			<th>  </th>
-			<th>Nume Playlist</th>
-			<th>Nume User</th>
-			<th>Dată Creare</th>
-		</tr>
-
-<?php
-if(mysqli_num_rows($result) > 0)
-{
-   while($row = mysqli_fetch_assoc($result))
-    {
-?>
-
-     	<tr>
-			<th><input type = 'radio' name = 'list_id' value = "<?php echo $row["lid"]?>"></th>
-          	<input  name="user_name" type="hidden" value = "<?php echo $username?>">
-            <th><?php echo $row["ltitle"]?></th>
-            <th><?php echo $row["username"]?></th>
-            <th><?php echo $row["lissuedate"]?></th>
+  <div id = "continut">
+    <h2>Playlists:</h2>
+    <form action="show_playlist.php" method = "POST">
+      <div id = "tabelPlusButon">
+      <table>
+        <tr>
+          <th>  </th>
+          <th>Nume Playlist</th>
+          <th>Nume User</th>
+          <th>Dată Creare</th>
         </tr>
-		
-<?php
-    }
-} 
-else 
-{
-    echo "Niciun Rezultat Găsit :(";
-}?></table>
-    
-    <br></br>
-    <input type = "submit"  name="list_select_button_2" value = "Selectează">
-    <br></br>
-    <a href="../Browser/search.php" >Înapoi la Pagina cu Rezultate</a>
-	
-</form>
+
+        <?php
+          if(mysqli_num_rows($result) > 0)
+          {
+            while($row = mysqli_fetch_assoc($result))
+              {
+        ?>
+        <tr>
+          <th><input type = 'radio' name = 'list_id' value = "<?php echo $row["lid"]?>"></th>
+          <input  name="user_name" type="hidden" value = "<?php echo $username?>">
+          <th><?php echo $row["ltitle"]?></th>
+          <th><?php echo $row["username"]?></th>
+          <th><?php echo $row["lissuedate"]?></th>
+        </tr>
+            
+        <?php
+            }
+        } 
+        else 
+        {
+            echo "Niciun Rezultat Găsit :(";
+        }?>
+      </table>
+          
+      <div id = "linkuri">
+        <input type = "submit"  name="list_select_button_2" value = "Selectează">
+        <a href="../Browser/search.php" >Înapoi la Pagina cu Rezultate</a>
+      </div>
+      </div>
+    </form>
+  </div>
 </body>
 
 </html>
-
 
 
 <!-- PHP ZONE! Don't touch it -->
