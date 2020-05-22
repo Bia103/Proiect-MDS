f92b74f5b4070f7cfd67cfdbff5aa083a78682d2
Oana-Ivan <49486605+Oana-Ivan@users.noreply.github.com>
2020-05-22 19:41:29 +0300

    Update create_list_success.php

--- Music Hall of Fame/PageOf/create_list_success.php
+++ Music Hall of Fame/PageOf/create_list_success.php
@@ -1,29 +1,41 @@
 <!-- PHP ZONE! Don't touch it -->
-<?php
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="Css/createListSuccess.css">
+    </head>
+    <body>
+        <div id = "continut">
+            <!-- PHP ZONE! Don't touch it -->
+            <?php
 
-$con = @mysqli_connect('localhost', 'root','root','proiectmds')
-    or die('Nu s-a putut efectua conectarea la baza de date. Eroare: ' . mysql_error());
-session_start();
+            $con = @mysqli_connect('localhost', 'root','root','proiectmds')
+                or die('Nu s-a putut efectua conectarea la baza de date. Eroare: ' . mysql_error());
+            session_start();
 
-$username = $_SESSION['username'];
-if(isset($_POST['confirm_create']) and !empty($_POST['list_title']))
-{
-    $list_title = $_POST['list_title'];
-    $yes = $_POST['yes'];
-    echo "Playlist creat cu Succes! Caută din nou melodia și pune-o în noul tău playlist. :)";
-    $result = mysqli_query($con, "INSERT INTO `list` (username, ltitle, lissuedate, ispublic) VALUES ('$username','$list_title', CURDATE(), $yes);");
-}
-else
-{
-    echo "Introdu Titlul Playlist-ului, te rog!";
-}
+            $username = $_SESSION['username'];
+            if(isset($_POST['confirm_create']) and !empty($_POST['list_title']))
+            {
+                $list_title = $_POST['list_title'];
+                $yes = $_POST['yes'];
+                echo "Playlist creat cu Succes! Caută din nou melodia și pune-o în noul tău playlist. :)";
+                $result = mysqli_query($con, "INSERT INTO `list` (username, ltitle, lissuedate, ispublic) VALUES ('$username','$list_title', CURDATE(), $yes);");
+            }
+            else
+            {
+                echo "Introdu Titlul Playlist-ului, te rog!";
+            }
 
-?>
+            ?>
 
 
-<br></br>
-<a href="../Browser/search.php" >Înapoi la Pagina cu Rezultate...</a>
-<br><br>
-<a href="../dashboard.php" >Înapoi la Pagina Principală...</a>
-<br><br>
-<a href="create_new_list.php" >Înapoi la Pagina de Creare Playlist...</a>
+            <div id = "linkuri">
+                <!-- <br></br> -->
+                <a href="../Browser/search.php" >Pagina cu Rezultate</a>
+                <!-- <br><br> -->
+                <a href="../dashboard.php" >Pagina Principală</a>
+                <!-- <br><br> -->
+                <a href="create_new_list.php" >Pagina de Creare Playlist</a>
+            </div>
+        </div>
+    </body>
+</html>
