70ac1f081e819f70187e2fb26859e39d60892b6c
StegarescuAnaMaria <48885998+StegarescuAnaMaria@users.noreply.github.com>
2020-06-14 22:42:49 +0300

    Add files via upload

---
+++ Music Hall of Fame/menu.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/css/menu.css">
+</head>
+<nav class>
+
+<ul class="container1">
+<li> <a href="/dashboard.php"> Home  </a></li>
+<li> <a href="/"> Log in  </a> </li>
+<li> <a href="//register.php"> Register  </a> </li>
+<li> <a href="/search.php"> Browse...  </a></li>
+</ul>
+</nav>
+</html>
